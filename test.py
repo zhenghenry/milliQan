d728f11495527d3f6f04d3c3b51805473ca61534
Henry Zheng <zhenghenry123@gmail.com>
2018-07-23 16:49:23 +0000

    Updated to include writing DAC inputs and ADC outputs to txt file

--- test.py
+++ test.py
@@ -1,26 +1,44 @@
 from periphery import I2C
 from time import sleep
 import sys
+import os
 
 i2c = I2C('/dev/i2c-6')
-def run(i2c,time,steps):
+def run(i2c,time,steps,Filename):
+	f = open(Filename + "_input.txt", "w+")
 	time = float(time)
 	volt = 0x0000
-	while(int(volt) < 2**16):
+	while(int(volt) < 26214):
 		i2c.transfer(0x20,[I2C.Message([0x3F,volt>>8, volt & 0xFF00>>8], read = False)])
 		volt = volt + steps
 		print('%.2f V' %(float(volt)*1.25/(2.**15)))
+		os.system("cat /sys/bus/iio/devices/iio:device0/in_voltage8_vpvn_raw >>" + Filename + "_output.txt")
+		f.write('%0.2f\n'%(float(volt)*1.25/(2.**15)))
+		f.flush()
+		os.fsync(f.fileno())
 		sleep(time)
-		if(int(volt) > 2**16 - steps - 1):
+		if(int(volt) > 26214 - steps - 1):
 			while(int(volt)>0):
 				volt = volt - steps
 				i2c.transfer(0x20,[I2C.Message([0x3F,volt>>8, volt & 0xFF00>>8], read = False)])
 				print('%.2f V' % (float(volt)*1.25/(2.**15)))
+				os.system("cat /sys/bus/iio/devices/iio:device0/in_voltage8_vpvn_raw >>" + Filename + "_output.txt")
+				f.write('%0.2f\n'%(float(volt)*1.25/(2.**15)))
+				f.flush()
+				os.fsync(f.fileno())
 				sleep(time)
 
 Time = sys.argv[1]
 Time = float(Time)
 Steps = float(sys.argv[2])
 Steps = int(Steps*2.**15/1.25)
+file_name = sys.argv[3]
+file_list = os.listdir(".")
+txt_list = []
+for names in file_list:
+	if names.endswith(".txt"):
+		txt_list.append(names)
+if file_name + "_output.txt" in txt_list:
+	raise Exception("File already exists!")
 while(True):
-	run(i2c,Time,Steps)
+	run(i2c,Time,Steps,file_name)
